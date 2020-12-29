7e54e7b94fab11fb45bc76c135907bea8eec055a
lsj8367 <lsj8367@naver.com>
2020-12-29 18:14:42 +0900

    Add Max3, Min3

---
+++ Algo/src/ex01/Q3.java
@@ -0,0 +1,33 @@
+package ex01;
+
+import java.util.Scanner;
+
+public class Q3 {
+	//네값의 최소값 구하기
+	static int min4(int a, int b, int c, int d) {
+		int min = a;
+		if(b < min) min = b;
+		if(c < min) min = c;
+		if(d < min) min = d;
+		
+		return min;
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("네값중 최소값 구하기");
+		
+		System.out.println("a : ");
+		int a = sc.nextInt();
+		System.out.println("b : ");
+		int b = sc.nextInt();
+		System.out.println("c : ");
+		int c = sc.nextInt();
+		System.out.println("d : ");
+		int d = sc.nextInt();
+		
+		int result = min4(a,b,c,d);
+		System.out.println("최소값은 " + result + " 입니다.");
+	}
+}
