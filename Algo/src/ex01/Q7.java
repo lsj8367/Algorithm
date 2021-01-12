78711c359804c9b4a15e3528e510c5dd541e9d35
lsj8367 <lsj8367@naver.com>
2021-01-12 17:51:58 +0900

    2021-01-12 commit

---
+++ Algo/src/ex01/Q7.java
@@ -0,0 +1,32 @@
+package ex01;
+
+import java.util.Scanner;
+
+public class Q7 {
+
+	public static void main(String[] args) {
+		// n을 입력받고 1 + 2 +... n = 전체합 구하기
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("1부터 n까지의 합을 구합니다.");
+		System.out.println("n의 값 : ");
+		int n = sc.nextInt();
+		
+		int sum = 0;
+		
+		for(int i = 1; i <= n; i++) {
+			//System.out.println(i);
+			
+			if(i < n) {
+				System.out.print(i + "+");
+			}else {
+				System.out.print(i);
+			}
+			
+			sum += i;
+		}
+		System.out.println("=" + sum);
+	}
+
+}
