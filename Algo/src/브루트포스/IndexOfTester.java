50199ed803aea9aa65f79a59419531c4ffc62352
lsj8367 <lsj8367@naver.com>
2021-08-31 19:16:54 +0900

    feat: 문자열 검색 실습

---
+++ Algo/src/브루트포스/IndexOfTester.java
@@ -0,0 +1,43 @@
+package 브루트포스;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
+public class IndexOfTester {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("텍스트 : ");
+        String text = sc.next();
+
+        System.out.println("패턴 : ");
+        String pattern = sc.next();
+
+        int idx1 = text.indexOf(pattern);
+        int idx2 = text.lastIndexOf(pattern);
+
+        if (idx1 == 1) {
+            System.out.println("텍스트 안에 패턴이 없습니다.");
+        } else {
+            int len1 = 0;
+            for (int i = 0; i < idx1; i++) {
+                len1 += text.substring(i, i + 1).getBytes().length;
+            }
+            len1 += pattern.length();
+
+            int len2 = 0;
+
+            for (int i = 0; i < idx2; i++) {
+                len2 += text.substring(i, i + 1).getBytes().length;
+            }
+
+            len2 += pattern.length();
+
+            System.out.println("텍스트 : " + text);
+            System.out.printf(String.format("패턴 %%%ds\n", len1), pattern);
+            System.out.println("텍스트 : " + text);
+            System.out.printf(String.format("패턴 %%%ds\n", len2), pattern);
+        }
+
+    }
+}
