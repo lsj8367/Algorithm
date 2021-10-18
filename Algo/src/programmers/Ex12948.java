4d8fe02d3cb89ad21226e437481b74f58465aae8
SeungJae Lee <lsj8367@naver.com>
2021-10-18 16:20:51 +0900

    feat: 핸드폰 번호 가리기 완료

---
+++ Algo/src/programmers/Ex12948.java
@@ -0,0 +1,22 @@
+package programmers;
+
+public class Ex12948 {
+
+    public static void main(String[] args) {
+        String ss = "01012345678";
+
+        Ex12948 ex12948 = new Ex12948();
+        ex12948.solution(ss);
+    }
+
+    public String solution(String phone_number) {
+        String frontStr = phone_number.substring(0, phone_number.length() - 4);
+
+        String answer = "";
+        for (int i = 0; i < frontStr.length(); i++) {
+            answer += "*";
+        }
+
+        return answer + phone_number.substring(phone_number.length() - 4);
+    }
+}
