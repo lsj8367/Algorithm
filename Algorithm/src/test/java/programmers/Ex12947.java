5779e56c5847c0d9df64cf6bbace781786926d11
SeungJae Lee <lsj8367@naver.com>
2021-10-20 10:15:34 +0900

    feat: 하샤드의 수 풀이

---
+++ Algorithm/src/test/java/programmers/Ex12947.java
@@ -0,0 +1,34 @@
+package programmers;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Ex12947 {
+    public boolean solution(int x) {
+        int sum = 0;
+        int a = x;
+
+        while (a >= 1) {
+            sum += a % 10;
+            a /= 10;
+        }
+
+        if (x % sum == 0) {
+            return true;
+        }
+
+        return false;
+    }
+
+    @Test
+    void test() {
+        assertThat(solution(10)).isTrue();
+        assertThat(solution(11)).isFalse();
+        assertThat(solution(12)).isTrue();
+        assertThat(solution(13)).isFalse();
+        assertThat(solution(14)).isFalse();
+        assertThat(solution(15)).isFalse();
+    }
+}
