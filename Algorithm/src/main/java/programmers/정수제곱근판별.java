0dec3b1f86ffec12af8a01caf66fe325577ce303
SeungJae Lee <lsj8367@naver.com>
2021-10-27 13:08:41 +0900

    feat: 정수제곱근판별 구현

---
+++ Algorithm/src/main/java/programmers/정수제곱근판별.java
@@ -0,0 +1,14 @@
+package programmers;
+
+public class 정수제곱근판별 {
+
+    public long solution(long n) {
+
+        Double sqrt = Math.sqrt(n);
+        if (sqrt == sqrt.intValue()) {
+            return (long) Math.pow(sqrt + 1, 2);
+        }
+
+        return -1;
+    }
+}
---
+++ Algorithm/src/test/java/programmers/Ex12934Test.java
@@ -0,0 +1,26 @@
+package programmers;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.stream.Stream;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+public class Ex12934Test {
+    정수제곱근판별 test = new 정수제곱근판별();
+
+    @ParameterizedTest
+    @MethodSource
+    void solution(int n, int expected) {
+        assertThat(test.solution(n)).isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> solution() {
+        return Stream.of(
+            Arguments.of(121, 144),
+            Arguments.of(3, -1)
+        );
+    }
+}
