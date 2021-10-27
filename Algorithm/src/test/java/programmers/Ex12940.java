d8ba281b998f70bb5c97fc885d47128d3b359f9e
SeungJae Lee <lsj8367@naver.com>
2021-10-27 12:45:39 +0900

    feat: 최대공약수, 최소공배수 구하기 완료

---
+++ Algorithm/src/test/java/programmers/Ex12940.java
@@ -0,0 +1,25 @@
+package programmers;
+
+public class Ex12940 {
+    public int[] solution(int n, int m) {
+
+        int[] answer = new int[2];
+
+        int gcd = gcd(n, m);
+
+        answer[0] = gcd;
+        answer[1] = (n * m) / gcd;
+
+        return answer;
+    }
+
+    private int gcd(int n, int m) {
+        while (m != 0) {
+            int temp = n % m;
+            n = m;
+            m = temp;
+        }
+
+        return n;
+    }
+}
---
+++ Algorithm/src/test/java/programmers/Ex12940Test.java
@@ -0,0 +1,31 @@
+package programmers;
+
+// 최대공약수, 최소공배수
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+public class Ex12940Test {
+
+    Ex12940 ex12940 = new Ex12940();
+
+
+
+    @ParameterizedTest
+    @MethodSource
+    void solution(int n, int m, int[] expected) {
+        assertThat(ex12940.solution(n, m)).isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> solution() {
+        return Stream.of(
+            Arguments.of(3, 12, new int[]{3, 12}),
+            Arguments.of(2, 5, new int[]{1, 10})
+        );
+    }
+
+}
