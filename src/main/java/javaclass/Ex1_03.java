1fd3e0c7d8ab062627408e14f77f3552fea3854d
lsj8367 <lsj8367@naver.com>
2022-01-03 22:25:06 +0900

    :sparkles: 섹션 1-03 문장 속 단어 해결

---
+++ src/main/java/javaclass/Ex1_03.java
@@ -0,0 +1,26 @@
+package javaclass;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Ex1_03 {
+
+    public static String solution(String[] input) {
+        String answer = "";
+
+        for (int i = 0; i < input.length; i++) {
+            if (answer.length() < input[i].length()) {
+                answer = input[i];
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+
+        System.out.println(solution(input));
+    }
+}
