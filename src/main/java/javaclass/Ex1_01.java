d60676458a765755b57fd57125eb2e20ad90c418
lsj8367 <lsj8367@naver.com>
2022-01-03 22:06:09 +0900

    :sparkles: 섹션 1-1 풀이

---
+++ src/main/java/javaclass/Ex1_01.java
@@ -0,0 +1,32 @@
+package javaclass;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Ex1_01 {
+
+    public static int solution(String inputStr, char spell) {
+        int count = 0;
+
+        inputStr = inputStr.toUpperCase();
+        spell = Character.toUpperCase(spell);
+
+        for (int i = 0; i < inputStr.length(); i++) {
+            if (spell == inputStr.charAt(i)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String inputStr = br.readLine();
+        char spell = br.readLine().charAt(0);
+
+        System.out.println(solution(inputStr, spell));
+    }
+
+}
