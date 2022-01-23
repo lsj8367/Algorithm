75f2af8f13f8b4d6e84c4018b21b6bc64f1ca7b4
lsj8367 <lsj8367@naver.com>
2022-01-23 15:52:47 +0900

    Revert "Revert ":sparkles: Ex3_04연속 부분수열 해결""
    This reverts commit e5941363bdafa23454eb04eb9426bc26766e174e.

---
+++ src/main/java/javaclass/ex3/Ex3_04.java
@@ -0,0 +1,60 @@
+package javaclass.ex3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Ex3_04 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int count = 0;
+
+//        for (int i = 0; i < n; i++) {
+//            int sum = 0;
+//
+//            for (int j = i; j < n; j++) {
+//                sum += arr[j];
+//                if (sum == m) {
+//                    count++;
+//                } else if (sum > m) {
+//                    break;
+//                }
+//            }
+//        }
+        int left = 0;
+
+        int sum = 0;
+
+        for (int right = 0; right < n; right++) {
+            sum += arr[right];
+            if (sum == m) {
+                count++;
+            }
+            while (sum >= m) {
+                sum -= arr[left];
+                left++;
+
+                if (sum == m) {
+                    count++;
+                }
+            }
+        }
+
+        System.out.println(count);
+    }
+
+}
