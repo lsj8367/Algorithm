6a48d56dcfe60865f9538ce1b018838ce1ee564b
lsj8367 <lsj8367@naver.com>
2022-01-24 22:24:42 +0900

    :sparkles: Ex3_06 연속 부분수열 해결

---
+++ src/main/java/javaclass/ex3/Ex3_06.java
@@ -0,0 +1,49 @@
+package javaclass.ex3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Ex3_06 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        System.out.println(solution(k, arr));
+    }
+
+    private static int solution(final int k, final int[] arr) {
+        int count = 0;
+        int left = 0;
+        int answer = 0;
+
+        for (int right = 0; right < arr.length; right++) {
+            if (arr[right] == 0) {
+                count++;
+            }
+
+            while (count > k) {
+                if (arr[left] == 0) {
+                    count--;
+                }
+
+                left++;
+            }
+
+            answer = Math.max(answer, right - left + 1);
+        }
+        return answer;
+    }
+
+}
