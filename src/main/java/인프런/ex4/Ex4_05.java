077701f7e8cc38365aa2069cb5a012bc4aac7592
lsj8367 <lsj8367@naver.com>
2022-01-30 12:49:04 +0900

    :sparkles: Ex4_05 k번째 큰 수 해결

---
+++ src/main/java/인프런/ex4/Ex4_05.java
@@ -0,0 +1,53 @@
+package 인프런.ex4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.Set;
+import java.util.StringTokenizer;
+import java.util.TreeSet;
+
+public class Ex4_05 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        System.out.println(solution(n, k, arr));
+    }
+
+    private static int solution(final int n, final int k, final int[] arr) {
+        int answer = -1;
+        Set<Integer> set = new TreeSet<>(Collections.reverseOrder());
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                for (int l = j + 1; l < n; l++) {
+                    set.add(arr[i] + arr[j] + arr[l]);
+                }
+            }
+        }
+
+        int count = 0;
+        for (Integer value : set) {
+            count++;
+            if (count == k) {
+                answer = value;
+            }
+        }
+
+        return answer;
+    }
+
+}
