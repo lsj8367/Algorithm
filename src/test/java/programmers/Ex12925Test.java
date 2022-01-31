719ae6ee6bc4498fccee012d4f01155d90efc344
lsj8367 <lsj8367@naver.com>
2022-01-31 15:29:04 +0900

    :white_check_mark: 12925 해결

---
+++ src/test/java/programmers/Ex12925Test.java
@@ -0,0 +1,13 @@
+package programmers;
+
+import org.junit.jupiter.api.Test;
+
+public class Ex12925Test {
+
+    @Test
+    void test() {
+        String aa = "+1234";
+        System.out.println();
+    }
+
+}
