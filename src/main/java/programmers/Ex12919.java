c01ab1dd02589929c12dc5a59dc1998983b3cc5a
lsj8367 <lsj8367@naver.com>
2022-02-02 23:59:43 +0900

    :sparkles: 서울에서 김서방 찾기 해결

---
+++ src/main/java/programmers/Ex12919.java
@@ -0,0 +1,28 @@
+package programmers;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Ex12919 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] arr = br.readLine().split(" ");
+
+        final int answer = solution(arr);
+
+        System.out.println("김서방은 " + answer + "에 있다");
+    }
+
+    private static int solution(String[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].equals("Kim")) {
+                return i;
+            }
+        }
+
+        return 0;
+    }
+
+}
