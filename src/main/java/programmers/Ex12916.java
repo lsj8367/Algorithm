5471290df3374d7e82ed3711f68118cf2688620b
lsj8367 <lsj8367@naver.com>
2022-02-05 15:48:24 +0900

    :sparkles: 12916 문자열 내 p,y개수

---
+++ src/main/java/programmers/Ex12916.java
@@ -0,0 +1,30 @@
+package programmers;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Locale;
+
+public class Ex12916 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String ss = br.readLine();
+
+        ss = ss.toLowerCase(Locale.ROOT);
+
+        int p = 0;
+        int y = 0;
+
+        for (int i = 0; i < ss.length(); i++) {
+            if (ss.charAt(i) == 'p') {
+                p++;
+            } else if (ss.charAt(i) == 'y') {
+                y++;
+            }
+        }
+
+        System.out.println(p == y);
+    }
+
+}
