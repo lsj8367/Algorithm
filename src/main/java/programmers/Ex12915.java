2141b8626606c19ebbe3f0891e9c20d81f42a59a
lsj8367 <lsj8367@naver.com>
2022-02-05 16:19:25 +0900

    :sparkles: 12915 문자열 내 마음대로 정렬하기

---
+++ src/main/java/programmers/Ex12915.java
@@ -0,0 +1,39 @@
+package programmers;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Ex12915 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        solution(new String[]{"sun", "bed", "car"}, n);
+
+    }
+
+    private static void solution(final String[] split, final int n) {
+        final String[] answer = Arrays.stream(split).sorted(new Comparator<String>() {
+            @Override
+            public int compare(final String o1, final String o2) {
+                if (o1.charAt(n) > o2.charAt(n)) {
+                    return 1;
+                }
+
+                if (o1.charAt(n) == o2.charAt(n)) {
+                    return o1.compareTo(o2);
+                }
+
+                return -1;
+            }
+        }).toArray(String[]::new);
+
+        for (String s : answer) {
+            System.out.println(s);
+        }
+    }
+
+}
