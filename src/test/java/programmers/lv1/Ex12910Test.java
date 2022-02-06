96f5c75c09c25514b8bb2dba951f5f1a3d60b3f5
lsj8367 <lsj8367@naver.com>
2022-02-06 18:00:09 +0900

    :white_check_mark: 테스트 케이스 추가

--- src/test/java/programmers/lv1/Ex12910Test.java
+++ src/test/java/programmers/lv1/Ex12910Test.java
@@ -1,13 +1,28 @@
 package programmers.lv1;
 
-import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class Ex12910Test {
 
-    @Test
-    void test() {
+    private static Stream<Arguments> solution() {
+        return Stream.of(
+            Arguments.of(new int[]{5, 9, 7, 10}, 5, new int[]{5, 10}),
+            Arguments.of(new int[]{2, 36, 1, 3}, 1, new int[]{1, 2, 3, 36}),
+            Arguments.of(new int[]{3, 2, 6}, 10, new int[]{-1})
+        );
+    }
+
+    @MethodSource
+    @ParameterizedTest
+    void solution(int[] arr, int divisor, int... values) {
         Ex12910 e = new Ex12910();
-        e.solution(new int[]{5, 9, 7, 10}, 5);
+        final int[] solution = e.solution(arr, divisor);
+
+        assertThat(solution).containsExactly(values);
     }
 
 }
