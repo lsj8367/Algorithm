578e01a61d8704866aa9d0747de6fe73b3afedf6
lsj8367 <lsj8367@naver.com>
2022-02-07 22:10:27 +0900

    :sparkles: 선택정렬

---
+++ src/main/java/인프런/ex6/SelectSort.java
@@ -0,0 +1,46 @@
+package 인프런.ex6;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class SelectSort {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        final int[] solution = solution(arr);
+        for (int i : solution) {
+            System.out.print(i + " ");
+        }
+    }
+
+    private static int[] solution(int[] arr) {
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIndex = i;
+
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+
+        return arr;
+    }
+
+}
