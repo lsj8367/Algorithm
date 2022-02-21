147e86c6326c08862122c3c5cc2b605d6878f4fd
lsj8367 <lsj8367@naver.com>
2022-02-21 22:26:43 +0900

    :recycle: 팩토리얼 재귀로 변경

--- src/main/java/재귀/Factorial_Q1.java
+++ src/main/java/재귀/Factorial_Q1.java
@@ -4,17 +4,18 @@ import java.util.Scanner;
 
 public class Factorial_Q1 {
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int targetNum = sc.nextInt();
-
-        int factorial = 1;
-
-        for (int i = 1; i <= targetNum; i++) {
-            factorial *= i;
+    private static int solution(final int n) {
+        if (n == 1) {
+            return 1;
         }
 
-        System.out.println(factorial);
+        return n * solution(n - 1);
     }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(solution(sc.nextInt()));
+    }
+
 
 }
