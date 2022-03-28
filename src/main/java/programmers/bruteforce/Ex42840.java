eddf53e86dcf648bb1c0a3d135a9f1efa0456a38
lsj8367 <lsj8367@naver.com>
2022-03-28 22:53:05 +0900

    :package: 패키지 변경

---
+++ src/main/java/programmers/bruteforce/Ex42840.java
@@ -0,0 +1,58 @@
+package programmers.bruteforce;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ex42840 {
+
+    //1번 1, 2, 3, 4, 5
+    //2번 2, 1, 2, 3, 2, 4, 2, 5
+    //3번 3, 3, 1, 1, 2, 2, 4, 4, 5, 5
+    private final int[] arr1 = new int[]{1, 2, 3, 4, 5};
+    private final int[] arr2 = new int[]{2, 1, 2, 3, 2, 4, 2, 5};
+    private final int[] arr3 = new int[]{3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+
+    public int[] solution(int[] answers) {
+        int[] count = new int[3];
+
+        for (int i = 0; i < answers.length; i++) {
+            int value = answers[i];
+
+            if (value == arr1[i % arr1.length]) {
+                count[0]++;
+            }
+
+            if (value == arr2[i % arr2.length]) {
+                count[1]++;
+            }
+
+            if (value == arr3[i % arr3.length]) {
+                count[2]++;
+            }
+
+        }
+
+        int maxPoint = 0;
+
+        for (int value : count) {
+            if (maxPoint < value) {
+                maxPoint = value;
+            }
+
+        }
+
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < count.length; i++) {
+
+            if (maxPoint == count[i]) {
+                list.add(i + 1);
+            }
+
+        }
+
+        return list.stream().mapToInt(answer -> answer)
+            .toArray();
+    }
+
+}
---
+++ src/test/java/programmers/bruteforce/Ex42840Test.java
@@ -0,0 +1,27 @@
+package programmers.bruteforce;
+
+import java.util.stream.Stream;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class Ex42840Test {
+
+    private final Ex42840 ex = new Ex42840();
+
+    private static Stream<Arguments> solution() {
+        return Stream.of(
+            Arguments.of(new int[]{1, 2, 3, 4, 5}, new int[]{1}),
+            Arguments.of(new int[]{1, 3, 2, 4, 2}, new int[]{1, 2, 3})
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void solution(final int[] answers, final int[] expected) {
+        final int[] actual = ex.solution(answers);
+        Assertions.assertThat(actual).containsExactly(expected);
+    }
+
+}
