8724e208efac494893ef6539b324f74b3ca5cd95
lsj8367 <lsj8367@naver.com>
2023-10-23 23:28:20 +0900

    feat: add ex 128871

--- src/main/java/programmers/lv1/Ex178871.java
+++ src/main/java/programmers/lv1/Ex178871.java
@@ -1,2 +1,29 @@
-package programmers.lv1;public class Ex178871 {
+package programmers.lv1;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Ex178871 {
+    public String[] solution(String[] players, String[] callings) {
+        Map<String, Integer> playersMap = new HashMap<>();
+
+        for (int i = 0; i < players.length; i++) {
+            playersMap.put(players[i], i);
+        }
+
+        for (String calling : callings) {
+            final Integer grade = playersMap.get(calling);
+            String front = players[grade - 1];
+
+            //swap
+            players[grade - 1] = calling;
+            playersMap.put(calling, grade - 1);
+
+            players[grade] = front;
+            playersMap.put(front, grade);
+        }
+
+        return players;
+    }
+
 }
--- src/test/java/programmers/lv1/Ex178871Test.java
+++ src/test/java/programmers/lv1/Ex178871Test.java
@@ -1,4 +1,21 @@
+package programmers.lv1;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class Ex178871Test {
-  
+
+    Ex178871 demo = new Ex178871();
+
+    @Test
+    void test() {
+        final String[] solution = demo.solution(new String[]{"mumu", "soe", "poe", "kai", "mine"},
+                new String[]{"kai", "kai", "mine", "mine"});
+
+        for (String s : solution) {
+            System.out.println(s);
+        }
+    }
+
 }
