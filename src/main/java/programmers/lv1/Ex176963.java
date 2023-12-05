a5a6a6ad7e67ca9569194661fb563ea1849e6dd3
lsj8367 <lsj8367@naver.com>
2023-12-06 01:25:16 +0900

    feat: 176963 추억점수

---
+++ src/main/java/programmers/lv1/Ex176963.java
@@ -0,0 +1,24 @@
+package programmers.lv1;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Ex176963 {
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        Map<String, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < name.length; i++) {
+            map.put(name[i], yearning[i]);
+        }
+
+        int[] answer = new int[photo.length];
+
+        for (int i = 0; i < photo.length; i++) {
+            for (int j = 0; j < photo[i].length; j++) {
+                answer[i] += map.getOrDefault(photo[i][j], 0);
+            }
+        }
+
+        return answer;
+    }
+}
